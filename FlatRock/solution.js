052558560672b6d4de8c4ac54d5fd9d3e1cc0893
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-10-06 23:41:21 +0300

    Small amendment

--- FlatRock/solution.js
+++ FlatRock/solution.js
@@ -16,10 +16,10 @@ function solution() {
     //iterating through all the 3 divs with class="item" to extract the needed productName, price and rating
     for (const item of items) {
         const currProductName = decodeHTML(item.querySelector('img').getAttribute('alt').trim());
-        const currPrice = item.querySelector('.price-display > span[style="display: none"]').textContent.trim().replace('$', '').replace(',', '')
+        const currPrice = item.querySelector('.price-display > span[style="display: none"]').textContent.trim().slice(1).replace(',', '')
         let currRating = parseFloat(item.getAttribute('rating'));
 
-     // scale down the ratings to normalize them to be out of 5
+     // scale down the ratings above 5 to normalize them to be out of 5
         if (currRating > 5) {
             let divider = 2;
             while ((currRating / divider) > 5) {
